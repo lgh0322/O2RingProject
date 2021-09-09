1b7ab5f657957ab5b6cfcf921507a458bc934e7f
李光浩 <liguanghao@viatomtech.com>
2021-09-09 15:06:07 +0800

    Initial commit

---
+++ app/src/main/java/com/vaca/o2ringproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vaca.o2ringproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
